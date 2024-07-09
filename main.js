5f64bfbfc03eeafc3746f9fd1f88aa8364bfbeff
YearaC <yearacho@gmail.com>
2024-07-10 01:23:08 +0800

    second

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //const API_KEY = `41109769dcc64e81a99117924af22df3`
 let news = [];
 const getLatestNews = async () => {
-    //const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=피부$page=1&pageSize=20&category=health`);
-   let url = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=암&page=1&pageSize=20&category=health`
+     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+   const url = new URL(`https://playful-kangaroo-f139ee.netlify.app/top-headlines`)
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
-    console.log("111rrr",data.articles);
+    console.log("rrr",data.articles);
 };
 
 getLatestNews();
