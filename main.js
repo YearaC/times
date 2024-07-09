320b874558ab4ddc8b979d16e00ef7ac84ac7f54
YearaC <yearacho@gmail.com>
2024-07-10 01:29:48 +0800

    net

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const API_KEY = `41109769dcc64e81a99117924af22df3`
 let news = [];
 const getLatestNews = async () => {
-     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
    const url = new URL(`https://playful-kangaroo-f139ee.netlify.app/top-headlines`)
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
-    console.log("rrr",data.articles);
+    console.log("rrr",news);
 };
 
 getLatestNews();
